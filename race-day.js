8ade4da62d41f25f3d6b53a3b29a5314c4a08d61
Benjamin-0012 <wanjalabenjaemma@gmail.com>
2023-05-02 21:05:36 +0300

    Race day

---
+++ race-day.js
@@ -0,0 +1,13 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = true;
+const age = 18;
+
+if (age > 18 && registeredEarly) {
+  console.log(`You will race at 9:30am with race number ${raceNumber += 1000}.`);
+} else if (age > 18 && !registeredEarly) {
+  console.log(`You will race at 11:00am with race number ${raceNumber += 1000}.`);
+} else if (age < 18){
+  console.log(`You will race at 12:30pm with rance number ${raceNumber}.`);
+} else {
+  console.log('See the registration desk.')
+}
