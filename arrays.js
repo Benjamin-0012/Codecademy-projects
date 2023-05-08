c92a3e101c1cb06d68f0c5ade0ab386698afd0ff
Benjamin-0012 <wanjalabenjaemma@gmail.com>
2023-05-08 15:32:45 +0300

    Arrays added

--- arrays.js
+++ arrays.js
@@ -63,3 +63,24 @@ console.log(groceryList);
 
 const pastaIndex = groceryList.indexOf('pasta');
 console.log(pastaIndex);
+
+
+// Arrays and Functions
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+console.log(concept);
+
+function removeElement(newArr) {
+  newArr.pop();
+}
+ removeElement(concept);
+ console.log(concept);
+
+//  Nested Arrays
+let numberClusters = [[1, 2], [3, 4], [5, 6]];
+const target = numberClusters[2][1]
