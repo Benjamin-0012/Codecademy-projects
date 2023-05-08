73e7bfac0bf9dddeb6a741b174c388b470440025
Benjamin-0012 <wanjalabenjaemma@gmail.com>
2023-05-08 23:32:40 +0300

    Loops

--- loops.js
+++ loops.js
@@ -8,6 +8,25 @@ for (let counter = 3; counter >= 0; counter--) {
     console.log(counter);
   }
   
+  // Looping through Arrays
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log(`I would love to visit ${vacationSpots[i]}`);
+}
+
+// Nested Loops
+let bobsFollowers = ['Isaac', 'David', 'Miracle', 'Shadrack'];
+let tinasFollowers = ['Mathew', 'Isaac', 'David'];
+let mutualFollowers = [];
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+}
+
+
   
    
   
