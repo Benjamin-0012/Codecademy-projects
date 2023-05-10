12beb3d2e0b833317c2629f03f20c524a894a60d
Benjamin-0012 <wanjalabenjaemma@gmail.com>
2023-05-10 16:03:32 +0300

    Break added

--- loops.js
+++ loops.js
@@ -50,6 +50,19 @@ let cupsAdded = 0;
  console.log(cupsAdded + ' cup was added') 
 } while (cupsAdded < cupsOfSugarNeeded);
 
-  
+// The break Keyword
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i])
+  if (rapperArray[i] === "Notorious B.I.G.") {
+  break;
+}
+}
+ console.log("Notorious B.I.G.");
+console.log("And if you don't know, now you know.");
+
+
+
    
   
